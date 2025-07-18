2a682f5f4c77138a0cd41fadad5f1f1abbaa1e92
elpreben <preben.nygard@gmail.com>
2025-07-18 15:36:19 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-// Tailwind config placeholder
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
